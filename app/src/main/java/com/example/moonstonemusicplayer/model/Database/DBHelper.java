f2f0c13db9254a9a94bea1c54ba6a41937000846
JohannesGrobelski <56040855+JohannesGrobelski@users.noreply.github.com>
2021-01-12 16:51:15 +0100

    Update DBHelper.java

--- app/src/main/java/com/example/moonstonemusicplayer/model/Database/DBHelper.java
+++ app/src/main/java/com/example/moonstonemusicplayer/model/Database/DBHelper.java
@@ -41,31 +41,66 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
 
     //Tabellennamen
+    static final String TABLE_FOLDER_LIST = "folder_list";
+    static final String TABLE_PLAYLIST_LIST = "favorite_list";
+    static final String TABLE_FAVORITE_LIST = "favorite_list";
+    static final String TABLE_RADIO_LIST = "radio_list";
     static final String TABLE_SONG_LIST = "song_list";
 
     //Variablen für die Tabellenspalten
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_TITLE = "Title";
-    public static final String COLUMN_ARTIST = "Artist";
-    public static final String COLUMN_URI = "Uri";
-    public static final String COLUMN_DURATION = "Duration";
-    public static final String COLUMN_LAST_POSITION = "LastPosition";
-    public static final String COLUMN_GENRE = "Genre";
-    public static final String COLUMN_LYRICS = "Lyrics";
-    public static final String COLUMN_MEANING = "Meaning";
+    public static final String FOLDER_COLUMN_ID = "_id";
+    public static final String FOLDER_COLUMN_PATH= "Path";
+    public static final String FOLDER_COLUMN_NAME = "Name";
+    public static final String FOLDER_COLUMN_TYPE = "Type";
 
-    //Create-Statement
+    public static final String PLAYLIST_COLUMN_ID = "_id";
+    public static final String PLAYLIST_COLUMN_SONG_ID = "SongID";
+
+    public static final String FAVORITE_COLUMN_ID = "_id";
+    public static final String FAVORITE_COLUMN_SONG_ID = "SongID";
+
+    public static final String RADIO_COLUMN_ID = "_id";
+    public static final String RADIO_COLUMN_SONG_ID = "SongID";
+
+    public static final String SONG_COLUMN_ID = "_id";
+    public static final String SONG_COLUMN_TITLE = "Title";
+    public static final String SONG_COLUMN_ARTIST = "Artist";
+    public static final String SONG_COLUMN_URI = "Uri";
+    public static final String SONG_COLUMN_DURATION = "Duration";
+    public static final String SONG_COLUMN_LAST_POSITION = "LastPosition";
+    public static final String SONG_COLUMN_GENRE = "Genre";
+    public static final String SONG_COLUMN_LYRICS = "Lyrics";
+    public static final String SONG_COLUMN_MEANING = "Meaning";
+
+    //Create-Statements
+    private static final String SQL_CREATE_TABLE_FOLDER =
+        "CREATE TABLE " + TABLE_FOLDER_LIST +
+            "(" + FOLDER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            FOLDER_COLUMN_PATH + " TEXT NOT NULL, " +
+            FOLDER_COLUMN_NAME + " TEXT NOT NULL, " +
+            FOLDER_COLUMN_TYPE + " TEXT NOT NULL)";
+
+    private static final String SQL_CREATE_TABLE_PLAYLIST =
+        "CREATE TABLE " + TABLE_PLAYLIST_LIST +
+            "(" + FOLDER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            awf + " TEXT NOT NULL, " +
+            FOLDER_COLUMN_NAME + " TEXT NOT NULL, " +
+            FOLDER_COLUMN_TYPE + " TEXT NOT NULL)";
+
+
     private static final String SQL_CREATE_TABLE_SONG =
             "CREATE TABLE " + TABLE_SONG_LIST +
-                    "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_TITLE + " TEXT NOT NULL, " +
-                    COLUMN_ARTIST + " TEXT NOT NULL, "  +
-                    COLUMN_URI + " TEXT NOT NULL, "  +
-                    COLUMN_DURATION + " INT NOT NULL, "  +
-                    COLUMN_LAST_POSITION + " INT NOT NULL, "  +
-                    COLUMN_GENRE + " TEXT NOT NULL, "  +
-                    COLUMN_LYRICS + " TEXT NOT NULL, "  +
-                    COLUMN_MEANING + " TEXT NOT NULL)";
+                    "(" + SONG_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    SONG_COLUMN_TITLE + " TEXT NOT NULL, " +
+                    SONG_COLUMN_ARTIST + " TEXT NOT NULL, "  +
+                    SONG_COLUMN_URI + " TEXT NOT NULL, "  +
+                    SONG_COLUMN_DURATION + " INT NOT NULL, "  +
+                    SONG_COLUMN_LAST_POSITION + " INT NOT NULL, "  +
+                    SONG_COLUMN_GENRE + " TEXT NOT NULL, "  +
+                    SONG_COLUMN_LYRICS + " TEXT NOT NULL, "  +
+                    SONG_COLUMN_MEANING + " TEXT NOT NULL)";
+
+
 
     //Drop-Statement
     private static final String SQL_DROP = "DROP TABLE IF EXISTS "+TABLE_SONG_LIST;
