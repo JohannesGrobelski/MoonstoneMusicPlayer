cf778130ce7b35420bd0b389905b671b1da1d72e
jgrobelski <grobelskijohannes@gmail.com>
2024-12-18 15:17:23 +0100

    add context menu item to add songs from playlist to another playlist

--- app/src/main/java/com/example/moonstonemusicplayer/controller/MainActivity/PlaylistFragment/PlaylistFragmentListener.java
+++ app/src/main/java/com/example/moonstonemusicplayer/controller/MainActivity/PlaylistFragment/PlaylistFragmentListener.java
@@ -2,12 +2,19 @@ package com.example.moonstonemusicplayer.controller.MainActivity.PlaylistFragmen
 
 import static com.example.moonstonemusicplayer.model.Database.Playlist.DBPlaylists.RECENTLY_ADDED_PLAYLIST_NAME;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.moonstonemusicplayer.R;
 import com.example.moonstonemusicplayer.model.Database.Playlist.DBPlaylists;
@@ -110,6 +117,7 @@ public class PlaylistFragmentListener implements AdapterView.OnItemClickListener
       //create menu item with groupid to distinguish between fragments
       //präsentation
       menu.add(1, 11, 0, "aus Playlist löschen");
+      menu.add(2, 11, 0, "zu Playlist hinzufügen");
       menu.getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
         @Override
         /** onContextItemSelected(MenuItem item) doesnt work*/
@@ -133,6 +141,20 @@ public class PlaylistFragmentListener implements AdapterView.OnItemClickListener
           return false;
         }
       });
+      menu.getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        @Override
+        /** onContextItemSelected(MenuItem item) doesnt work*/
+        public boolean onMenuItemClick(MenuItem item) {
+          AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+          int index = info.position;
+          Song song = playListFragment.getPlaylistManager().getCurrentPlaylist().getPlaylist().get(index);
+
+          showAlertDialogAddToPlaylists(playListFragment.getLayoutInflater(), playListFragment.getContext(), song);
+
+          return false;
+        }
+      });
+
     } else {//the menu created if a playlist is clicked on
       menu.add(1, 12, 0, "playlist löschen");
       menu.getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
@@ -212,4 +234,40 @@ public class PlaylistFragmentListener implements AdapterView.OnItemClickListener
       }
     }).start();
   }
+
+
+  private void showAlertDialogAddToPlaylists(LayoutInflater inflater, Context context, final Song song){
+    final String[] allPlaylistNames = DBPlaylists.getInstance(context).getAllPlaylistNames();
+
+    View dialogView = inflater.inflate(R.layout.add_to_playlist_layout, null);
+    ListView lv_playlist_alert = dialogView.findViewById(R.id.lv_playlists_alert);
+    final EditText et_addNewPlaylist = dialogView.findViewById(R.id.et_addNewPlaylist);
+
+    lv_playlist_alert.setAdapter(new ArrayAdapter<String>(context,android.R.layout.simple_list_item_1,allPlaylistNames));
+
+    final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+    dialogBuilder.setView(dialogView);
+    dialogBuilder.setNegativeButton(android.R.string.no,null);
+    dialogBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        String text = et_addNewPlaylist.getText().toString();
+        if(!text.isEmpty()){
+          DBPlaylists.getInstance(context).addToPlaylist(context,song,text);
+        }
+      }
+    });
+    dialogBuilder.setTitle("Füge den Song einer Playlist hinzu \noder erstelle eine neue.");
+
+    final AlertDialog alertDialog  = dialogBuilder.show();
+
+    lv_playlist_alert.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        DBPlaylists.getInstance(context).addToPlaylist(context,song,allPlaylistNames[position]);
+        alertDialog.dismiss();
+      }
+    });
+  }
+
 }
